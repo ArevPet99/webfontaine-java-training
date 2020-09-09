9780c610b3edaa7dc2292478fb5eaa6f5a203079
apetoyan <petoyanarev@gmail.com>
2020-09-09 14:43:59 +0400

    Added 1 from Homework1

---
+++ src/Homework1/TemperatureConverter.java
@@ -0,0 +1,13 @@
+package Homework1;
+
+import java.util.Scanner;
+
+public class TemperatureConverter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input a degree in Fahrenheit:  ");
+        float input = scanner.nextInt();
+        float result = (input - 32) * 5 / 9;
+        System.out.println(input + " degree Fahrenheit is equal to " + result + " in Celsius");
+    }
+}
