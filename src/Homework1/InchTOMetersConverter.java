1af275bde26a0421f46ce1d8abb9c31a2a236294
apetoyan <petoyanarev@gmail.com>
2020-09-09 14:58:36 +0400

    Added second exercise from Homework1

---
+++ src/Homework1/InchTOMetersConverter.java
@@ -0,0 +1,13 @@
+package Homework1;
+
+import java.util.Scanner;
+
+public class InchTOMetersConverter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input a value for inch:  ");
+        double inputInch = scanner.nextInt();
+        double resultInmeters = inputInch * 0.0254;
+        System.out.println(inputInch + " inch is " + resultInmeters + " meters");
+    }
+}
